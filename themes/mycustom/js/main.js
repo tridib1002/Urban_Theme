22193fb08d4410ef285c742876c20c198efc31ba
Tridib Ghosh <tridib.ghosh@innoraft.com>
2017-05-18 11:03:15 +0530

    First commit after creating homepage HTML

---
+++ themes/mycustom/js/main.js
@@ -0,0 +1,12 @@
+(function($){ 
+$(".nav li.expanded").hover(
+    function(){
+      $(this).addClass("open");
+    },function(){
+      $(this).removeClass("open");
+    }
+  );
+})(jQuery); 
+
+
+
