8e8ff1539ad91a179381b2445167b4a02145f529
Tridib Ghosh <tridibghoshce@gmail.com>
2017-06-06 19:05:42 +0530

    portfolio all views added

--- themes/mycustom/js/main.js
+++ themes/mycustom/js/main.js
@@ -2,6 +2,8 @@
   // PORTFOLIO
 
   var $grid = $('.block-views-blockour-work-block-1');
+  var $grid2 = $('.block-views-blockour-work-portfolio-block-1');
+  var $grid3 = $('.block-views-blockportfolio-grid-block-1');
 
   $grid.imagesLoaded( function(){
     $grid.isotope({
@@ -13,7 +15,29 @@
       }
     })
   });
- //
+
+  $grid2.imagesLoaded( function(){
+    $grid2.isotope({
+      itemSelector: '.views-field-field-product-image',
+      percentPosition: true,
+      masonry: {
+        // use outer width of grid-sizer for columnWidth
+        columnWidth: '.views-field-field-product-image'
+      }
+    })
+  });
+
+  $grid3.imagesLoaded( function(){
+    $grid3.isotope({
+      itemSelector: '.views-field-field-product-image',
+      percentPosition: true,
+      masonry: {
+        // use outer width of grid-sizer for columnWidth
+        columnWidth: '.views-field-field-product-image'
+      }
+    })
+  });
+
  //  // filter items on button click
  //  $('.portfolio-menu').on( 'click', 'button', function() {
  //    var filterValue = $(this).attr('data-filter');
